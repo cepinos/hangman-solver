0a59581e8cc4e0b2dcc70bf731a648480ebcbff4
Cesar Pino <cesarpino547@gmail.com>
2015-12-20 02:08:39 -0500

    Words.txt grouped by words length

--- README.md
+++ README.md
@@ -2,5 +2,12 @@
 
 A hangman game script solver.
 
+## Approach used
+
+The game gives a few parameters to consider, one of them is the length. Since the length is a fix variable, in order to maximize the performance of the algorimth, the script should look for it in a reduced dictionary of words with the same length, in this case the length of the word the script is guessing. The dictionary words.txt was splitted in multiple files inside the folder words, every file contains words with the same length, the name of the file is the length of the words. The script used to split the dictionary is groupwords.js.
+
+```
+node groupwords.js
+```
 
 Copyright: Cesar Pino @ 2015
--- groupwords.js
+++ groupwords.js
@@ -5,11 +5,31 @@ fs.readFile('./words.txt', function (err, data) {
   if (err) {
     throw err;
   }
-  var words = data.toString().split('\n');
+  // Initialize vars
+  var i,
+  words;
 
-  for( var i = 0; i < words.length; i++){
-    console.log(words[i]);
+  // Delete previous files
+  deleteWordsFiles();
+
+  // parse words dictionary to array
+  words = data.toString().split('\n');
+
+  for(i = 0; i < words.length; i++){
     fs.appendFileSync('words/' + words[i].length + '.txt', words[i] + '\n');
   }
 
 });
+
+function deleteWordsFiles(){
+  var i,
+  path = '';
+  // Delete files to avoid duplicated words
+  for(i = 0; i < 50; i++){
+    path = 'words/' + i + '.txt';
+    if (fs.existsSync(path)) {
+      fs.unlinkSync(path);
+    }
+
+  }
+}
