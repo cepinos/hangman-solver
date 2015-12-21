ad74b8dd8c93c1a4140f8c7c175d178795a15733
Cesar Pino <cesar.pino@inqbation.com>
2015-12-21 02:44:34 -0500

    Solver object ready and tested

--- hangmansolver/app/scripts/hangmansolver.js
+++ hangmansolver/app/scripts/hangmansolver.js
@@ -5,42 +5,34 @@
  */
 var Solver = (function($){
   // Initialize object-like class
-  var Solver = function(){};
+  var Solver = function(){},
+
+  // private functions
+  /**
+   * Create regexp from game input
+   */
+  parseGuessingWordtoRegExp = function(exp){
+    exp = exp.replace(/\*/g,'.');
+    return new RegExp('^' + exp + '$','g');
+  };
+
 
   // Attributes
+  Solver.prototype.dictionary = null;
   Solver.prototype.letters = {
-    'a': 0,
-    'b': 0,
-    'c': 0,
-    'd': 0,
-    'e': 0,
-    'f': 0,
-    'g': 0,
-    'h': 0,
-    'i': 0,
-    'j': 0,
-    'k': 0,
-    'l': 0,
-    'm': 0,
-    'n': 0,
-    'o': 0,
-    'p': 0,
-    'q': 0,
-    'r': 0,
-    's': 0,
-    't': 0,
-    'u': 0,
-    'v': 0,
-    'w': 0,
-    'x': 0,
-    'y': 0,
-    'z': 0,
+    'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0,
+    'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0,
+    's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0
   };
+  Solver.prototype.lettersSorted = [];
   Solver.prototype.word = '';
   Solver.prototype.words = [];
-  Solver.prototype.dictionary = null;
 
   // Methods
+  /**
+   * get words.txt from server and save it in dictironary attributte
+   * @param {Function} done callback called when the dictionary have the data from words.txt
+   */
   Solver.prototype.setDictionary = function(done){
     var _this = this;
     $.get('/words.txt').done(function(data){
@@ -53,11 +45,58 @@ var Solver = (function($){
 
     });
   }
-  Solver.prototype.getWords = function(regex){
+  /**
+   * Filter words of a given length
+   * @param  {number} length [length of the words to be filter]
+   * @return {array} [words filtered]
+   */
+  Solver.prototype.getWordsOfLength = function(length){
+    var regex = new RegExp('^.{' + length + '}$', 'gm');
+    this.words = this.dictionary.match(regex);
+    return this.words;
   }
+  /**
+   * Filter words that follow the pattern given by the Game
+   * @param  {string} exp [the word given by the game (i.e, '*a*k')]
+   * @return {array} [filtered list]
+   */
+  Solver.prototype.filterWords = function(exp){
+    var regex = parseGuessingWordtoRegExp(exp);
+
+    this.words = this.words.filter(function(word){
+      var match = word.match(regex);
+      return !!match && match.constructor === Array;
+    });
+
+    return this.words;
+  }
+  /**
+   * count words including a given letter
+   * @return {array} array with the amount of words containing a letter
+   */
   Solver.prototype.countLetters = function(){
+    var letter, keys, _this = this;
+    for(letter in this.letters){
+      if (this.letters.hasOwnProperty(letter)) {
+        this.letters[letter] = this.words.reduce(function(previousValue, word){
+          if (word.indexOf(letter) !== -1){
+            return previousValue + 1;
+          }
+          return previousValue;
+        }, 0);
+      }
+    }
+    return this.letters;
   }
+  /**
+   * sort list of letters by value
+   * @return {array} keys sorted
+   */
   Solver.prototype.sort = function(){
+    var list = this.letters,
+    keys = Object.keys(list);
+    this.lettersSorted = keys.sort(function(a,b){return list[a]-list[b]})
+    return this.lettersSorted;
   }
 
   return Solver;
@@ -65,5 +104,10 @@ var Solver = (function($){
 
 
 var HangmanSolver = (function(G, S){
+  // private attributes
+  var solver,
+  // private methods
+  initialize = function(){
 
+  }
 })(Game, Solver);
--- hangmansolver/test/spec/hangmansolver.spec.js
+++ hangmansolver/test/spec/hangmansolver.spec.js
@@ -1,3 +1,4 @@
+/* global Solver */
 (function () {
   'use strict';
 
@@ -17,10 +18,108 @@
       it('Should get words.txt from server and save it in the object solver', function () {
         expect(solver.dictionary.constructor === String).toBe(true);
       });
-      xit('Should get words from dictionary should return an array', function () {
-        var words = solver.getWords();
+      it('Should get words given a length from dictionary and return an array with all words with the same length', function () {
+        var words = solver.getWordsOfLength(3);
         expect(words.constructor === Array).toBe(true);
+        for( var i = 0; i < words.length; i++){
+          expect(words[i].length === 3).toBe(true);
+        }
+      });
+      it('Should filter words given a pattern like "*a*k" and it may return words like "park" or "mark"', function () {
+        var words, match;
+        // initialize words of length 4
+        solver.getWordsOfLength(4);
+        words = solver.filterWords('*a*k');
+        expect(words.constructor === Array).toBe(true);
+
+        for (var i = 0; i < words.length; i++) {
+          match = words[i].match(/.a.k/g);
+          expect(match.constructor === Array).toBe(true);
+        }
+      });
+      it('Should count the words including a letter, and save it in an array', function () {
+        var letters;
+
+        solver.words = [
+          'asy',
+          'cat',
+          'dog',
+          'fet',
+          'fda',
+          'get',
+          'hat',
+          'jet'
+        ];
+        letters = solver.countLetters();
+
+        expect(letters.a).toBe(4);
+        expect(letters.b).toBe(0);
+        expect(letters.c).toBe(1);
+        expect(letters.d).toBe(2);
+        expect(letters.e).toBe(3);
+        expect(letters.f).toBe(2);
+        expect(letters.g).toBe(2);
+        expect(letters.h).toBe(1);
+        expect(letters.i).toBe(0);
+        expect(letters.j).toBe(1);
+        expect(letters.k).toBe(0);
+        expect(letters.l).toBe(0);
+        expect(letters.m).toBe(0);
+        expect(letters.n).toBe(0);
+        expect(letters.o).toBe(1);
+        expect(letters.p).toBe(0);
+        expect(letters.q).toBe(0);
+        expect(letters.r).toBe(0);
+        expect(letters.s).toBe(1);
+        expect(letters.t).toBe(5);
+        expect(letters.u).toBe(0);
+        expect(letters.v).toBe(0);
+        expect(letters.w).toBe(0);
+        expect(letters.x).toBe(0);
+        expect(letters.y).toBe(1);
+        expect(letters.z).toBe(0);
+
+      });
+      it('Should sort letters', function () {
+        var lettersSorted = [];
+        solver.letters = {
+          'a': 4,
+          'b': 0,
+          'c': 1,
+          'd': 2,
+          'e': 3,
+          'f': 2,
+          'g': 2,
+          'h': 1,
+          'i': 0,
+          'j': 1,
+          'k': 0,
+          'l': 0,
+          'm': 0,
+          'n': 0,
+          'o': 1,
+          'p': 0,
+          'q': 0,
+          'r': 0,
+          's': 1,
+          't': 5,
+          'u': 0,
+          'v': 0,
+          'w': 0,
+          'x': 0,
+          'y': 1,
+          'z': 0
+        };
+
+        lettersSorted = solver.sort();
+
+        expect(lettersSorted.pop()).toBe('t');
+        expect(lettersSorted.pop()).toBe('a');
+        expect(lettersSorted.pop()).toBe('e');
+
       });
     });
   });
 })();
+
+
